1182d4e9a3c73fb52003b6aba9faaf1d6c453795
AbdulHannan-123 <75980674+AbdulHannan-123@users.noreply.github.com>
2024-02-17 17:18:16 +0500

    Search bar in home screen

--- lib/controllers/homecontroller.dart
+++ lib/controllers/homecontroller.dart
@@ -2,10 +2,10 @@ import 'package:api_calling_getx/models/postmodel.dart';
 import 'package:api_calling_getx/services/remote_services.dart';
 import 'package:get/state_manager.dart';
 
-class HomeController extends GetxController{
-
-   var isLoading = true.obs;
+class HomeController extends GetxController {
+  var isLoading = true.obs;
   var productList = <Temperatures>[].obs;
+  var filteredProductList = <Temperatures>[].obs;
 
   @override
   void onInit() {
@@ -18,10 +18,21 @@ class HomeController extends GetxController{
       isLoading(true);
       var products = await RemoteServices.fetchProducts();
       if (products.isNotEmpty) {
-        productList.value = products;
+        productList.assignAll(products);
+        filteredProductList.assignAll(products);
       }
     } finally {
       isLoading(false);
+    }
+  }
+
+  void search(String query) {
+    if (query.isEmpty) {
+      filteredProductList.assignAll(productList);
+    } else {
+      filteredProductList.assignAll(productList.where((product) => 
+        product.name!.toLowerCase().contains(query.toLowerCase()) ||
+        product.original!.toLowerCase().contains(query.toLowerCase())));
     }
   }
 }
--- lib/views/homapage.dart
+++ lib/views/homapage.dart
@@ -16,10 +16,10 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("Spoonacular"),
+        title: const Text("Spoonacular"),
         actions: [
           IconButton(
-            icon:const Icon(Icons.favorite),
+            icon: const Icon(Icons.favorite),
             onPressed: () {
               Navigator.of(context).push(MaterialPageRoute(builder: (context) => FetchTemperaturesScreen(),));
             },
@@ -28,107 +28,114 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
-        child: Obx(
-          () {
-            if (productController.isLoading.value) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              return GridView.builder(
-                gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                ),
-                itemCount: productController.productList.length,
-                itemBuilder: (context, index) {
-                  var product = productController.productList[index];
-                  final indexinlist = itemController.temperatureList.indexWhere((t) => t.id == productController.productList[index].id);
-                  if (indexinlist != -1) {
-                    product.isFavorite.value = true;
-                  //   await _prefsService.removeTemperature(index);
-                  //   temperatureList.removeAt(index);
-                  }
-                  return GestureDetector(
-                    onTap: () {
-                      // Handle product tap
-                    },
-                    child: Card(
-                      elevation: 2,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                             Stack(
-              children: [
-                Container(
-                  height: 60,
-                  width: double.infinity,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Image.network(
-                    product.image!,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-               
-                  Positioned(
-                  right: 0,
-                  child: Obx(() => CircleAvatar(
-                        backgroundColor: Colors.white,
-                        child: IconButton(
-                          icon:
-                           product.isFavorite.value 
-                              ?const Icon(Icons.favorite_rounded)
-                              :const Icon(Icons.favorite_border),
-                          onPressed: () {
-                            product.isFavorite.toggle();
-                            product.isFavorite.value
-                            ?itemController.saveTemperature(product)
-                            :itemController.removeTemperature(product);
-
-                          },
-                        ),
-                      )),
-                )
-              ],
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) {
+                productController.search(value);
+              },
+              decoration: InputDecoration(
+                hintText: 'Search...',
+              ),
             ),
-            const SizedBox(height: 8),
-                            Text(
-                              product.name ?? "",
-                              maxLines: 1,
-                              style:const TextStyle(
-                                fontFamily: 'avenir',
-                                fontWeight: FontWeight.w800,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              product.original ?? "",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style:const TextStyle(
-                                fontSize: 16,
-                                fontFamily: 'avenir',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+            const SizedBox(height: 15),
+            Obx(() {
+              if (productController.isLoading.value) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                return Expanded(
+                  child: GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
                     ),
-                  );
-                },
-              );
-            }
-          },
+                    itemCount: productController.filteredProductList.length,
+                    itemBuilder: (context, index) {
+                      var product =productController.filteredProductList.isEmpty?productController.productList[index] :productController.filteredProductList[index];
+                      final indexInList = itemController.temperatureList.indexWhere((t) => t.id == product.id);
+                      if (indexInList != -1) {
+                        product.isFavorite.value = true;
+                      }
+                      return GestureDetector(
+                        onTap: () {
+                          // Handle product tap
+                        },
+                        child: Card(
+                          elevation: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Stack(
+                                  children: [
+                                    Container(
+                                      height: 60,
+                                      width: double.infinity,
+                                      clipBehavior: Clip.antiAlias,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(4),
+                                      ),
+                                      child: Image.network(
+                                        product.image!,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    Positioned(
+                                      right: 0,
+                                      child: Obx(() => CircleAvatar(
+                                        backgroundColor: Colors.white,
+                                        child: IconButton(
+                                          icon: product.isFavorite.value 
+                                            ? const Icon(Icons.favorite_rounded)
+                                            : const Icon(Icons.favorite_border),
+                                          onPressed: () {
+                                            product.isFavorite.toggle();
+                                            product.isFavorite.value 
+                                              ? itemController.saveTemperature(product)
+                                              : itemController.removeTemperature(product);
+                                          },
+                                        ),
+                                      )),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  product.name ?? "",
+                                  maxLines: 1,
+                                  style: const TextStyle(
+                                    fontFamily: 'avenir',
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  product.original ?? "",
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontFamily: 'avenir',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              }
+            }),
+          ],
         ),
       ),
     );
   }
 }
-
